e60f56c631804f53dc1d1bd53b86ac165217f043
Stuart Yamartino <stuart@yamartino.com>
2015-03-26 19:40:19 -0400

    Affixing side guide nav

--- resources/views/guide/show.blade.php
+++ resources/views/guide/show.blade.php
@@ -1,14 +1,17 @@
 @extends('app')
 
 @section('content')
+    <div class="guide-affix">
+        <ul>
+            @foreach($index as $each)
+                <li><a href="#{{ $each['id'] }}">{{ $each['title'] }}</a></li>
+            @endforeach
+        </ul>
+    </div>
     <div class="container">
         <div class="row">
             <div class="col-md-3">
-                <ul>
-                    @foreach($index as $each)
-                        <li><a href="#{{ $each['id'] }}">{{ $each['title'] }}</a></li>
-                    @endforeach
-                </ul>
+
             </div>
             <div class="col-md-9">
 
