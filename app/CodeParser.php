e83e76b1afff906c2e52fe53b1f8a4d5085d46ac
Stuart Yamartino <stuart@yamartino.com>
2015-03-26 21:41:12 -0400

    Made page anchors always the same

--- app/CodeParser.php
+++ app/CodeParser.php
@@ -1,12 +1,10 @@
 <?php namespace Style;
 
-use Illuminate\Support\Str;
-
 class CodeParser {
 
   public function H1ToList($html){
     $html = preg_replace_callback('/<h1>(.+)<\/h1>/', function($val){
-      $save = Str::random(6);
+      $save = substr(md5($val[1]), 25);
       GuideIndex::setArray(['id'=>$save, 'title'=>$val[1]]);
       return "<a name='$save'></a>$val[0]";
     }, $html);
